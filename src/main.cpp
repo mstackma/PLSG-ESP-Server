1e4b5203651fc5c2b5fc722eb6e1701c590b6054
mstackma <marvin_sebastian.stackmann@smail.th-koeln.de>
2024-01-09 14:32:19 +0100

    added button handleClick handleMotor

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
+//Editing of the ESP32 Server - SPIFFS - version:
 /*********
- * Own editing of the original ESP32 Server - SPIFFS - version of:
   Rui Santos
   Complete project details at https://RandomNerdTutorials.com/esp32-esp8266-input-data-html-form/
   
@@ -9,15 +9,26 @@
   The above copyright notice and this permission notice shall be included in all
   copies or substantial portions of the Software.
 *********/
-
 #include <Arduino.h>
 
-int ledPinLeft = 15;
-int ledPinRight = 2;
+#define ledPinLeft 2
+#define ledPinRight 15
 #define motorLeftPin1 33
 #define motorLeftPin2 32
-#define motorRightPin1 35
-#define motorRightPin2 34
+//#define motorRightPin1 35
+//#define motorRightPin2 34
+#define buttonPin 4
+
+unsigned long startTime = 0;
+unsigned long duration = 0;
+
+const int thresholdLight = 600;
+
+// Variables will change:
+int motorConectionState = LOW;// the current state of the motor-sensor-connection
+int buttonState = 0;             // the current reading from the input pin
+int lastButtonState = 0;   // the previous reading from the input pin
+int cm = 0;
 int motorLeft;
 int motorRight;
 
@@ -54,11 +65,11 @@ const char index_html[] PROGMEM = R"rawliteral(
     }
   </script></head><body>
   <form action="/get" target="hidden-form">
-    inputInt1 (current value %inputInt1%): <input type="number1 " name="inputInt1">
+    inputInt1 (current value %inputInt1%): <input type="number" name="inputInt1">
     <input type="submit" value="Submit" onclick="submitMessage()">
   </form><br>
   <form action="/get" target="hidden-form">
-    inputInt2 (current value %inputInt2%): <input type="number2 " name="inputInt2">
+    inputInt2 (current value %inputInt2%): <input type="number" name="inputInt2">
     <input type="submit" value="Submit" onclick="submitMessage()">
   </form>
   <iframe style="display:none" name="hidden-form"></iframe>
@@ -69,19 +80,19 @@ void notFound(AsyncWebServerRequest *request) {
 }
 
 String readFile(fs::FS &fs, const char * path){
-  Serial.printf("Reading file: %s\r\n", path);
+  //Serial.printf("Reading file: %s\r\n", path);
   File file = fs.open(path, "r");
   if(!file || file.isDirectory()){
     Serial.println("- empty file or failed to open file");
     return String();
   }
-  Serial.println("- read from file:");
+  //Serial.println("- read from file:");
   String fileContent;
   while(file.available()){
     fileContent+=String((char)file.read());
   }
   file.close();
-  Serial.println(fileContent);
+  //Serial.println(fileContent);
   return fileContent;
 }
 
@@ -112,13 +123,98 @@ String processor(const String& var){
   return String();
 }
 
+void handleClick()
+{
+  if (buttonState != lastButtonState)
+  {
+    if (buttonState == HIGH)
+    {
+      startTime = millis();
+    }
+    else
+    {
+      duration = millis() - startTime;
+
+      if (duration < 500)
+      { // short button press
+        motorConectionState = !motorConectionState;
+        Serial.println("-----------------------------short------------------------------------");
+      }
+      else
+      { // long button press
+        motorConectionState = LOW;
+        Serial.println("-----------------------------long-------------------------------------");
+      }
+    }
+  }
+
+  lastButtonState = buttonState;
+}
+/*
+//following function from https://www.electronicscuriosities.com/2020/10/ultrasonic-sensor-with-arduino-uno-with.html
+long readUltrasonicDistance(int triggerPin, int echoPin)
+{
+  pinMode(triggerPin, OUTPUT);  // Clear the trigger
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+
+  // Sets the trigger pin to HIGH state for 10 microseconds
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  pinMode(echoPin, INPUT);
+  
+  // Reads the echo pin, and returns the sound wave travel time in microseconds
+  return pulseIn(echoPin, HIGH);
+}
+*/
+//to react when an obstacle is nearby
+void handleMotor() {
+  // measure the ping time in cm
+  
+  /*
+  cm = 0.01723 * readUltrasonicDistance(7, 8);
+  Serial.println(cm);
+
+  if (cm < 6){
+    digitalWrite(motorLeftPin1,LOW);    //backwards
+    digitalWrite(motorLeftPin2,HIGH);
+    //digitalWrite(motorRightPin1,LOW);
+    //digitalWrite(motorRightPin2,HIGH);
+    delay(2000);
+  } */
+  if (motorConectionState == 1) {
+    analogWrite(ledPinLeft, motorLeft);
+    analogWrite(ledPinRight, motorRight);
+
+    analogWrite(motorLeftPin1, motorLeft);  //forwards
+    digitalWrite(motorLeftPin2,LOW);
+    //analogWrite(motorRightPin1, motorRight);
+    //digitalWrite(motorRightPin2,LOW);
+  } else {
+    analogWrite(ledPinLeft,0);
+    analogWrite(ledPinRight,0);
+
+    digitalWrite(motorLeftPin1,LOW);  //stop
+    digitalWrite(motorLeftPin2,LOW);
+    //digitalWrite(motorRightPin1,LOW);
+    //digitalWrite(motorRightPin2,LOW);
+  }
+}
+
 void setup() {
   Serial.begin(115200);
 
+  pinMode(ledPinLeft, OUTPUT);
+  digitalWrite(ledPinLeft, LOW);
+  pinMode(ledPinRight, OUTPUT);
+  digitalWrite(ledPinRight, LOW);
   pinMode(motorLeftPin1, OUTPUT);
   pinMode(motorLeftPin2, OUTPUT);
-  pinMode(motorRightPin1, OUTPUT);
-  pinMode(motorRightPin2, OUTPUT);
+  //pinMode(motorRightPin1, OUTPUT);
+  //pinMode(motorRightPin2, OUTPUT);
+
+  pinMode(buttonPin, INPUT);
 
   // Initialize SPIFFS
   #ifdef ESP32
@@ -164,29 +260,33 @@ void setup() {
     else {
       inputMessage = "No message sent";
     }
-    Serial.println(inputMessage);
+    //Serial.println(inputMessage);
     request->send(200, "text/text", inputMessage);
   });
   server.onNotFound(notFound);
   server.begin();
 }
 
-void loop() {
+void loop() {  
   // To access your stored values on inputInt
   int yourInputInt1 = readFile(SPIFFS, "/inputInt1.txt").toInt();
-  Serial.print("*** Your inputInt1: ");
-  Serial.println(yourInputInt1);
+  //Serial.print("*** Your inputInt1: ");
+  //Serial.println(yourInputInt1);
   
   int yourInputInt2 = readFile(SPIFFS, "/inputInt2.txt").toInt();
-  Serial.print("*** Your inputInt2: ");
-  Serial.println(yourInputInt2);
+  //Serial.print("*** Your inputInt2: ");
+  //Serial.println(yourInputInt2);
 
-  digitalWrite(motorLeftPin2,LOW);
-  digitalWrite(motorRightPin2,LOW);
-  analogWrite(ledPinLeft, yourInputInt1);
-  analogWrite(ledPinRight, yourInputInt2);
-  analogWrite(motorLeftPin1, yourInputInt1);
-  analogWrite(motorLeftPin2, yourInputInt2);
+  if (yourInputInt1 <= 255) {
+    motorLeft = yourInputInt1;
+  }
+  if (yourInputInt2 <= 255) {
+    motorRight = yourInputInt2;
+  }
+
+  buttonState = digitalRead(buttonPin);
+  handleClick();
+  handleMotor();
   
-  delay(1000);
+  //delay(5000);
 }
